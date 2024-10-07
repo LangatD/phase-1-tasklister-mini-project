590e674668ff5ee3d48760128a88fedfb2b6505e
LangatD <dorcaschepngetich20@gmail.com>
2024-10-08 00:06:58 +0300

    updated todolist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+      // Get the new task from the input 
+      const newTask = document.getElementById('new-task-description').value;
+
+      // Create a new list item for the task
+      const listItem = document.createElement('li');
+      listItem.textContent = newTask;
+       
+      // delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => {
+        listItem.remove();
+      });
+      taskList.appendChild(listItem);
+      listItem.appendChild(deleteButton);
+ 
+      form.reset();
+  });
 });
